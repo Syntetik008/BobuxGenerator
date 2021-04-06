555d37cfa0bb62c29a331e9bd2fab73edd1c9c44
Wungiel <Bartek.Jan.Wandzel@gMail.com>
2021-04-07 00:49:38 +0200

    yes, free bobux

---
+++ main.py
@@ -0,0 +1,20 @@
+#imports
+import random
+import sys
+
+loopnum = 0
+
+many = input("How many to generate: ")  # asks for how many codes to generate
+
+f= open("output.txt","w+")  # creating output.txt
+
+while loopnum < int(many):  # loop
+    result_str = ''.join((random.choice('1234567890') for i in range(3)))  # generate code
+    result_str2 = ''.join((random.choice('1234567890') for i in range(3)))  # generate code
+    result_str3 = ''.join((random.choice('1234567890') for i in range(3)))
+    result_str4 = ''.join((random.choice('1234567890') for i in range(3)))
+    print(result_str + " " + result_str2 + " " + result_str3 + " " + result_str4)  # show codes
+    f.write(result_str + " " + result_str2 + " " + result_str3 + " " + result_str4 + "\n")  # adding codes to output.txt
+    loopnum += 1
+
+input("ok? (press any button)")
