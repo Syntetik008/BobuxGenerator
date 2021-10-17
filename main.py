033a7367df7fb229dda27ceb94c523ea528e4c2f
Wungiel <47087989+Syntetik008@users.noreply.github.com>
2021-10-17 16:38:18 +0200

    notes :3

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ many = input("How many to generate: ")  # asks for how many codes to generate
 f= open("output.txt","w+")  # creating output.txt
 
 while loopnum < int(many):  # loop
-    result_str = ''.join((random.choice('1234567890') for i in range(3)))  # generate code
-    result_str2 = ''.join((random.choice('1234567890') for i in range(3)))  # generate code
-    result_str3 = ''.join((random.choice('1234567890') for i in range(3)))
-    result_str4 = ''.join((random.choice('1234567890') for i in range(3)))
+    result_str = ''.join((random.choice('1234567890') for i in range(3)))  # generate code 1
+    result_str2 = ''.join((random.choice('1234567890') for i in range(3)))  # generate code 2
+    result_str3 = ''.join((random.choice('1234567890') for i in range(3)))  # generate code 3
+    result_str4 = ''.join((random.choice('1234567890') for i in range(3)))  # generate code 4
     print(result_str + " " + result_str2 + " " + result_str3 + " " + result_str4)  # show codes
     f.write(result_str + " " + result_str2 + " " + result_str3 + " " + result_str4 + "\n")  # adding codes to output.txt
-    loopnum += 1
+    loopnum += 1    # add 1 to loop number
 
-input("ok? (press any button)")
+input("ok? (press any button)") # closing window
